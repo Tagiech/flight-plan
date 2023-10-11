7e7d4aa86232c8d89294ccb9caac20dc36b84644
Artur Tagiev <a.tagiev@dodopizza.com>
2023-10-11 11:17:10 +0300

    changed extraction logic from strict xpaths to serial table extraction based on headers colors

--- repository/workplan_repository.py
+++ repository/workplan_repository.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
@@ -27,12 +28,9 @@ class WorkPlan(object):
 
         self.__login(login, password)
 
-        flights = self.__get_flights()
-        flights.extend(self.__get_early_flights())
-        reserves = self.__get_reserves()
-        events = self.__get_work_events()
+        flights, reserves, work_events = self.__get_events()
 
-        return flights, reserves, events
+        return flights, reserves, work_events
 
     def __login(self, login, password):
         try:
@@ -54,39 +52,48 @@ class WorkPlan(object):
         except:
             raise Exception("Login failed")
 
-    def __get_flights(self) -> list[Flight]:
+    def __get_events(self) -> (list[Flight], list[Reserve], list[WorkEvent]):
+        flights: list[Flight] = []
+        reserves: list[Reserve] = []
+        work_events: list[WorkEvent] = []
+
+        basic_route: str = '/html/body/div[3]/div/div[4]'
+        base_element = self.service.find_element(By.XPATH, basic_route)
+        tables = base_element.find_elements(By.CSS_SELECTOR, 'div.panel.panel-info')
+
+        for table in tables:
+            table_header = table.find_element(By.CSS_SELECTOR, 'div.panel-heading')
+            header_style = table_header.get_attribute("style")
+            if "rgb(176, 214, 237)" in header_style or "rgb(202, 236, 151)" in header_style:
+                element = table.find_element(By.CSS_SELECTOR, 'tbody.noPrint')
+                flights.extend(self.__get_flights(element))
+            elif "rgb(245, 240, 146)" in header_style:
+                element = table.find_element(By.CSS_SELECTOR, 'tbody')
+                reserves.extend(self.__get_reserves(element))
+            elif "rgb(222, 199, 236)" in header_style:
+                element = table.find_element(By.CSS_SELECTOR, 'tbody')
+                work_events.extend(self.__get_work_events(element))
+
+        return flights, reserves, work_events
+
+    def __get_flights(self, element: WebElement) -> list[Flight]:
         try:
-            element = self.service.find_element(By.XPATH,
-                                                '/html/body/div[3]/div/div[4]/div[2]/div[2]/table/tbody[2]')
             html = bs(element.get_attribute('innerHTML'), 'html.parser')
             flights = self._convert.to_flight(html)
         except:
             return []
         return flights
 
-    def __get_early_flights(self) -> list[Flight]:
+    def __get_reserves(self, element: WebElement) -> list[Reserve]:
         try:
-            element = self.service.find_element(By.XPATH,
-                                                '/html/body/div[3]/div/div[4]/div[3]/div[2]/table/tbody[1]')
-            html = bs(element.get_attribute('innerHTML'), 'html.parser')
-            flights = self._convert.to_flight(html)
-        except:
-            return []
-        return flights
-
-    def __get_reserves(self) -> list[Reserve]:
-        try:
-            element = self.service.find_element(By.XPATH,
-                                                '/html/body/div[3]/div/div[4]/div[4]/div[2]/table/tbody')
             html = bs(element.get_attribute('innerHTML'), 'html.parser')
             reserves = self._convert.to_reserve(html)
         except:
             return []
         return reserves
 
-    def __get_work_events(self) -> list[WorkEvent]:
+    def __get_work_events(self, element: WebElement) -> list[WorkEvent]:
         try:
-            element = self.service.find_element(By.XPATH, '/html/body/div[3]/div/div[4]/div[5]/div[2]/table/tbody')
             html = bs(element.get_attribute('innerHTML'), 'html.parser')
             events = self._convert.to_work_event(html)
         except:
